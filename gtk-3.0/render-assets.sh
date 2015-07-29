e67b495de08d1add810b404fe73114891deee541
0x414c! <0x414c@gmail.com>
2015-07-29 20:26:28 +0300

    gtk-3.0: Enabled OptiPNG in render-assets.sh.

--- gtk-3.0/render-assets.sh
+++ gtk-3.0/render-assets.sh
@@ -7,28 +7,36 @@ SRC_FILE="assets.svg"
 ASSETS_DIR="assets"
 INDEX="assets.txt"
 
+
+if [ -f $ASSETS_DIR ]; then
+    echo $ASSETS_DIR exists.
+else
+    mkdir $ASSETS_DIR
+    echo Output directory $ASSETS_DIR created.
+fi
+
 for i in `cat $INDEX`
-do 
+do
 if [ -f $ASSETS_DIR/$i.png ]; then
     echo $ASSETS_DIR/$i.png exists.
 else
-    echo
-    echo Rendering $ASSETS_DIR/$i.png
+    echo Rendering $i to $ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
               --export-id-only \
-              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null #\
-    # && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
+              --export-png=$ASSETS_DIR/$i.png $SRC_FILE > /dev/null \
+    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png
 fi
 if [ -f $ASSETS_DIR/$i@2.png ]; then
     echo $ASSETS_DIR/$i@2.png exists.
 else
-    echo
-    echo Rendering $ASSETS_DIR/$i@2.png
+    echo Rendering $i to $ASSETS_DIR/$i@2.png
     $INKSCAPE --export-id=$i \
               --export-dpi=180 \
               --export-id-only \
-              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null #\
-    # && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png 
+              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE > /dev/null \
+    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png
 fi
 done
+
 exit 0
+
